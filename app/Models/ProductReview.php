e3365adcf4e95bc0d5a262fd0aafa90a0dba348c
dev-roni <dev.ronisingha@gmail.com>
2026-06-10 14:12:49 +0600

    product review model created

--- app/Models/ProductReview.php
+++ app/Models/ProductReview.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProductReview extends Model
 {
-    //
+    protected $fillable = [
+        'product_id', 'user_id', 'rating', 'title', 'body', 'is_approved',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
