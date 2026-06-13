1bcce37ea4fe9d484175f07fe16e420d078e47b8
dev-roni <dev.ronisingha@gmail.com>
2026-06-13 16:40:11 +0600

    migration add for payment field

---
+++ database/migrations/2026_05_17_075906_add_payment_fields_to_orders_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+{
+    Schema::table('orders', function (Blueprint $table) {
+        $table->string('ssl_transaction_id')->nullable()->after('transaction_id');
+        $table->string('ssl_val_id')->nullable()->after('ssl_transaction_id');
+        $table->json('ssl_response')->nullable()->after('ssl_val_id');
+    });
+}
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            //
+        });
+    }
+};
