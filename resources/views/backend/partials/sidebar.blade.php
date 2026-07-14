475d94aaed682c7281d8ef02f7864b5feea3cb22
dev-roni <dev.ronisingha@gmail.com>
2026-07-14 15:17:22 +0600

    feat(admin pannel): sidebar menu active fixed

--- resources/views/backend/partials/sidebar.blade.php
+++ resources/views/backend/partials/sidebar.blade.php
@@ -1,23 +1,50 @@
     <div class="overlay" id="overlay"></div>
     <!-- সাইডবার -->
     <nav class="sidebar" id="sidebar">
-        <div class="sidebar-header"><h3>ই-শপ অ্যাডমিন</h3><span>CMS প্যানেল</span></div>
+        <div class="sidebar-header">
+            <span><a href="{{route('shop.index')}}" class="text-decoration-none text-reset" target="_blank"><i class="fa fa-home" aria-hidden="true"></i> Browse Site</a></span>
+            <h3>E Shop Admin</h3>
+        </div>
         <ul class="sidebar-menu">
-            <li><a href="{{route('admin.dashboard')}}" class="active"><i class="fas fa-tachometer-alt"></i> ড্যাশবোর্ড</a></li>
+            <li>
+                <a href="{{ route('admin.dashboard') }}"
+                class="nav-link {{ request()->routeIs('admin.dashboard') ? 'active' : '' }}">
+                    <i class="fas fa-tachometer-alt"></i> Dashboard
+                </a>
+            </li>
             <li>  
                 <a href="{{ route('admin.sales.report') }}"class="nav-link {{ request()->routeIs('admin.sales.*') ? 'active' : '' }}">
                     <i class="fas fa-chart-line"></i> Sales Report
                 </a>
             </li>
-            <li><a href="{{route('admin.products.index')}}"><i class="fas fa-box"></i> পণ্যসমূহ</a></li>
-            <li><a href="{{route('admin.products.create')}}"><i class="fas fa-plus"></i> পণ্য যুক্ত করুন</a></li>
-            <li><a href="{{route('admin.attributes.index')}}"><i class="fa-solid fa-sitemap"></i> পণ্যের ভেরিয়েন্ট</a></li>
-            <li><a href="{{route('admin.categories.index')}}"><i class="fas fa-layer-group"></i> ক্যাটাগরি সমূহ</a></li>
-            <li><a href="{{route('admin.categories.create')}}"><i class="fas fa-plus-circle"></i> ক্যাটাগরি তৈরি</a></li>
-
+            <li>  
+                <a href="{{ route('admin.products.index') }}"class="nav-link {{ request()->routeIs('admin.products.index') ? 'active' : '' }}">
+                    <i class="fas fa-box"></i> Products Manage
+                </a>
+            </li>
+            <li>  
+                <a href="{{ route('admin.products.create') }}"class="nav-link {{ request()->routeIs('admin.products.create') ? 'active' : '' }}">
+                    <i class="fas fa-plus"></i> Product Add
+                </a>
+            </li>
+            <li>  
+                <a href="{{ route('admin.attributes.index') }}"class="nav-link {{ request()->routeIs('admin.attributes.index') ? 'active' : '' }}">
+                    <i class="fa-solid fa-sitemap"></i> Product Varient
+                </a>
+            </li>
+            <li>  
+                <a href="{{ route('admin.categories.index') }}"class="nav-link {{ request()->routeIs('admin.categories.index') ? 'active' : '' }}">
+                    <i class="fas fa-layer-group"></i> Category Manage
+                </a>
+            </li>
+            <li>  
+                <a href="{{ route('admin.categories.create') }}"class="nav-link {{ request()->routeIs('admin.categories.create') ? 'active' : '' }}">
+                    <i class="fas fa-plus-circle"></i> Category Create
+                </a>
+            </li>
             <li> <a href="{{ route('admin.orders.index') }}"
                 class="nav-link {{ request()->routeIs('admin.orders.*') ? 'active' : '' }}">
-                    <i class="fas fa-shopping-cart"></i> অর্ডার সমূহ
+                    <i class="fas fa-shopping-cart"></i> Orders
                     @if(($pendingCount = \App\Models\Order::where('status','pending')->count()) > 0)
                         <span class="badge bg-danger ms-1">{{ $pendingCount }}</span>
                     @endif
@@ -60,13 +87,13 @@
                     <i class="fas fa-users"></i> Customers
                 </a>
             </li>
-            <li><a href="settings.html"><i class="fas fa-cog"></i> সেটিংস</a></li>
+            <li><a href="settings.html"><i class="fas fa-cog"></i> Settings</a></li>
             <li>
                 <form method="POST" action="{{ route('logout') }}">
                     @csrf
 
                     <button type="submit" class="border-0 bg-transparent text-white w-100 text-start p-0">
-                        <i class="fas fa-sign-out-alt"></i> লগআউট
+                        <i class="fas fa-sign-out-alt"></i> Logout
                     </button>
                 </form>
             </li>
