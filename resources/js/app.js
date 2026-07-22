0be9973ab0fe8a1e86150e9368e60e1227dcef72
dev-roni <dev.ronisingha@gmail.com>
2026-07-22 13:15:06 +0600

    Move Font Awesome from CDN to Vite

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,4 @@
+import '../css/app.css';
 import './bootstrap';
 import * as bootstrap from 'bootstrap';
 window.bootstrap = bootstrap;
--- resources/views/backend/partials/header.blade.php
+++ resources/views/backend/partials/header.blade.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'পবনবাহিকা')</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
     @vite(['resources/css/admin.css','resources/js/admin.js'])
     
 </head>
