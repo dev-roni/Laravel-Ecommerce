478d84c46b4a1782b58910367067a718427a255f
dev-roni <dev.ronisingha@gmail.com>
2026-07-22 15:54:41 +0600

    feat(security): securityheader middleware csp divided for local and production, vite config host change

--- app/Http/Middleware/SecurityHeaders.php
+++ app/Http/Middleware/SecurityHeaders.php
@@ -8,11 +8,6 @@ use Symfony\Component\HttpFoundation\Response;
 
 class SecurityHeaders
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  Closure(Request): (Response)  $next
-     */
     public function handle(Request $request, Closure $next): Response
     {
         $response = $next($request);
@@ -23,6 +18,8 @@ class SecurityHeaders
         $response->headers->set(
             'X-Frame-Options', 'SAMEORIGIN'
         );
+
+        // X-XSS-Protection আধুনিক ব্রাউজারে Deprecated
         $response->headers->set(
             'X-XSS-Protection', '1; mode=block'
         );
@@ -32,14 +29,59 @@ class SecurityHeaders
         $response->headers->set(
             'Permissions-Policy', 'camera=(), microphone=(), geolocation=()'
         );
+
+
+        if (app()->isLocal()) {
+
+            // Development (Vite)
+            $csp = "
+                default-src 'self';
+
+                script-src 'self' 'unsafe-inline' 'unsafe-eval'
+                    http://127.0.0.1:5173
+                    https://cdn.jsdelivr.net;
+
+                style-src 'self' 'unsafe-inline'
+                    http://127.0.0.1:5173
+                    https://fonts.googleapis.com;
+
+                font-src 'self'
+                    data:
+                    http://127.0.0.1:5173
+                    https://fonts.gstatic.com;
+                img-src 'self'
+                    data:
+                    blob:
+                    http://localhost:8000
+                    http://127.0.0.1:8000
+                    https://lh3.googleusercontent.com
+                    https://fastly.picsum.photos/seed/admin/40/40
+                    https://picsum.photos/seed/admin/40/40;
+                connect-src 'self'
+                    http://localhost:5173
+                    http://127.0.0.1:5173
+                    http://[::1]:5173
+                    ws://localhost:5173
+                    ws://127.0.0.1:5173
+                    ws://[::1]:5173;
+            ";
+
+        } else {
+
+            // Production
+            $csp = "
+                default-src 'self'; 
+                script-src 'self' 'unsafe-inline' cdn.jsdelivr.net fonts.googleapis.com; 
+                style-src 'self' 'unsafe-inline' cdn.jsdelivr.net fonts.googleapis.com; 
+                font-src 'self' fonts.gstatic.com cdn.jsdelivr.net;
+                img-src 'self' data: lh3.googleusercontent.com;
+                connect-src 'self';
+            ";
+        }
+
         $response->headers->set(
             'Content-Security-Policy',
-            "default-src 'self'; " .
-            "script-src 'self' 'unsafe-inline' cdn.jsdelivr.net fonts.googleapis.com; " .
-            "style-src 'self' 'unsafe-inline' cdn.jsdelivr.net fonts.googleapis.com; " .
-            "font-src 'self' fonts.gstatic.com cdn.jsdelivr.net; " .
-            "img-src 'self' data: lh3.googleusercontent.com; " .
-            "connect-src 'self';"
+            preg_replace('/\s+/', ' ', trim($csp))
         );
 
         return $response;
