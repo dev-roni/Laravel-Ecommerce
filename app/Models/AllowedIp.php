5762f64301c0ea9cb24d56c97af2c4a6f3606fbe
dev-roni <dev.ronisingha@gmail.com>
2026-08-06 19:29:51 +0600

    feat(allowed ip cache): add model event booted method in allowed ip model for clear chache

--- app/Models/AllowedIp.php
+++ app/Models/AllowedIp.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Cache;
 
 class AllowedIp extends Model
 {
@@ -13,4 +14,16 @@ class AllowedIp extends Model
         'is_active',
         
     ];
+
+    protected static function booted(): void
+    {
+        foreach(['created','updated','delated'] as $event){
+            static::$event(
+                function(){
+                    Cache::forgot('allowed_ips');
+                }
+            );
+
+        }
+    }
 }
