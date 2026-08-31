8e8afdc03c825467682adde6fd68748d42f4d47c
dev-roni <dev.ronisingha@gmail.com>
2026-08-31 20:23:35 +0600

    feat(logging user): logging add in authentication logout

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -40,6 +40,11 @@ class AuthenticatedSessionController extends Controller
      */
     public function destroy(Request $request): RedirectResponse
     {
+        AuditService::log(
+            'user.logout',
+            auth()->user()
+        );
+        
         Auth::guard('web')->logout();
 
         $request->session()->invalidate();
