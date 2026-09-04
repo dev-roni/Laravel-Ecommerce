a0a13306b49f15b1cafa1e187695ce99cac504b9
dev-roni <dev.ronisingha@gmail.com>
2026-09-04 13:36:49 +0600

    feat(audit log clean): make schedule for git logs clean

--- routes/console.php
+++ routes/console.php
@@ -8,5 +8,8 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-//Schedule command for Cleanin Idempotency key
+//Schedule command for Cleaning Idempotency key
 Schedule::command('idempotency:cleanup')->dailyAt('06.00');
+
+// schedule command for cleaning audit logs
+Schedule::command('audit:cleanup --days=90')->monthly();
